c101e55cb96475c925c940d5cd479850a6a1e6fb
José Victor Tavares de Moura <jose.moura@hivecloud.com.br>
2020-04-10 15:51:09 -0300

    Ajustes JPAConfiguration

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,28 +3,30 @@ package br.com.casadocodigo.loja.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 public class ProdutosController {
-	
+
 	//Anotacao responsavel por injetar o DAO dentro do Controller
 	@Autowired
-	private ProdutoDAO produtoDao;
-	
-	@RequestMapping("/produtos/form")
-	public String form(){
-		return "produtos/form";
-	}
+	private ProdutoDAO dao;
 
+	@RequestMapping("/produtos/form")
+	public ModelAndView form() {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		return modelAndView;
+	}
+	
 	@RequestMapping("/produtos")
 	public String gravar(Produto produto){
 		System.out.println(produto);
-		produtoDao.gravar(produto);
-		
-		return "produtos/ok";
+		dao.gravar(produto);
+		return "/produtos/ok";
 	}
-	
 }
