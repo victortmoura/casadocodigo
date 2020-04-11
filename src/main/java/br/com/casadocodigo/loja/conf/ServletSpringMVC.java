52b4ec546de9276a1ace9f8c8e86c8b5fabe4ef3
José Victor Tavares de Moura <jose.moura@hivecloud.com.br>
2020-04-11 14:06:41 -0300

    ServeletSpringMVC: Override getServletFilters lidar com enconding no bdd

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -1,5 +1,9 @@
 package br.com.casadocodigo.loja.conf;
 
+import javax.servlet.Filter;
+
+import org.hibernate.annotations.Filters;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -20,4 +24,11 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		return new String[] {"/"};
 	}
 
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		
+		return new Filter[] {encodingFilter};
+	}
 }
